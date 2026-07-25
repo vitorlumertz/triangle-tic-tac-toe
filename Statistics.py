9c53874a50f26d10ba054bea3fdf5853dff66263
vitorlumertz <vitorlumertz@gmail.com>
2026-07-24 22:15:10 -0300

    first commit

---
+++ Statistics.py
@@ -0,0 +1,86 @@
+from TriangleTicTacToe import *
+from itertools import product
+import json
+
+
+def GeneratePositions():
+  characters = ['x', 'A', 'B']
+  positions = set()
+
+  for combination in product(characters, repeat=9):
+    string = ''
+    for c in combination:
+      string += c
+    numA = string.count('A')
+    numB = string.count('B')
+    diff = numA - numB
+    if (numA <= 3) and (numB <= 3) and (diff >= 0) and (diff <= 1):
+      positions.add(string)
+
+  return positions
+
+
+def GetGameFromPositionString(posString):
+  p1 = Player('Advanced', True)
+  p2 = Player('Advanced', False)
+  game = Game(p1,p2)
+
+  boardPositions = 'abcdefghi'
+
+  for i, c in enumerate(posString):
+    if c == 'A':
+      p1.SetMove(('x', boardPositions[i]))
+    elif c == 'B':
+      p2.SetMove(('x', boardPositions[i]))
+
+  return game
+
+
+def GetBestMoves():
+  bestMoves = dict()
+  positions = GeneratePositions()
+  for p in positions:
+    game = GetGameFromPositionString(p)
+    p1 = game.player1
+    p2 = game.player2
+
+    if game.IsP1Winner():
+      bestMoves.update({p: {'P1': 'Winner', 'P2': 'Loser'}})
+      continue
+
+    if game.IsP2Winner():
+      bestMoves.update({p: {'P1': 'Loser', 'P2': 'Winner'}})
+      continue
+
+    numA = p.count('A')
+    numB = p.count('B')
+
+    Round = 'Both'
+    if numA > numB:
+      Round = 'P2'
+    elif numA < 3:
+      Round = 'P1'
+
+    p1BestMoves = None
+    p2BestMoves = None
+    if (Round == 'P1') or (Round == 'Both'):
+      p1BestMoves = p1.BestMoves(game, p2)
+    if (Round == 'P2') or (Round == 'Both'):
+      p2BestMoves = p2.BestMoves(game, p1)
+
+    bestMoves.update({p: {'P1': p1BestMoves, 'P2': p2BestMoves}})
+    print(p, bestMoves[p])
+
+  return bestMoves
+
+
+def SaveBestMoves():
+  bestMoves = GetBestMoves()
+  bestMovesString = json.dumps(bestMoves, sort_keys=True, indent=2)
+  file = open('BestMoves2.json', 'a')
+  file.write(bestMovesString)
+  file.close()
+
+
+if __name__ == '__main__':
+  SaveBestMoves()
---
+++ TriangleTicTacToe.py
@@ -0,0 +1,336 @@
+import random
+import time
+import json
+
+
+def RemoveNones(t: tuple):
+  return [val for val in t if val is not None]
+
+
+def GetPositionString(p1Positions, p2Positions):
+  posString = ''
+  boardPositions = 'abcdefghi'
+  for c in boardPositions:
+    if c in p1Positions:
+      posString += 'A'
+    elif c in p2Positions:
+      posString += 'B'
+    else:
+      posString += 'x'
+
+  return posString
+
+
+class Player:
+  def __init__(self, level, isP1, bestMovesFileName='BestMoves.json'):
+    self.positions = [None for i in range(3)]
+    self.level = level
+    self.isP1 = isP1
+    self.bestMoves = None
+    if level == 'FastAdvanced':
+      file = open(bestMovesFileName)
+      bestMoves = json.load(file)
+      self.bestMoves = bestMoves
+
+  def PossibleMoves(self, game, opponentPositions):
+    impossiblePositions = RemoveNones(self.positions + opponentPositions)
+
+    if None in self.positions:
+      possibleMoves = [('x', p) for p in game.positions if p not in impossiblePositions]
+      return possibleMoves
+
+    possibleMoves = list()
+    for oldPosition in self.positions:
+      pieceMoves = [(oldPosition, newPosition) for newPosition in game.connections[oldPosition] if newPosition not in impossiblePositions]
+      possibleMoves.extend(pieceMoves)
+    return possibleMoves
+
+
+  def SetMove(self, move):
+    oldPosition = move[0]
+    newPosition = move[1]
+
+    if oldPosition == 'x':
+      i = self.positions.index(None)
+    else:
+      i = self.positions.index(oldPosition)
+
+    self.positions[i] = newPosition
+
+
+  def ResetMove(self, move):
+    oldPosition = move[0]
+    newPosition = move[1]
+
+    i = self.positions.index(newPosition)
+
+    if oldPosition == 'x':
+      self.positions[i] = None
+    else:
+      self.positions[i] = oldPosition
+
+
+  def __twoMovesFilter(self, game, opponent):
+    possibleMoves = self.PossibleMoves(game, opponent.positions)
+    filteredPossibleMoves = list()
+
+    for move in possibleMoves:
+      self.SetMove(move)
+
+      if game.IsWinner(self.positions):
+        self.ResetMove(move)
+        return [move]
+      else:
+        opponentPms = opponent.PossibleMoves(game, self.positions)
+        for m in opponentPms:
+          opponent.SetMove(m)
+          if game.IsWinner(opponent.positions):
+            opponent.ResetMove(m)
+            break
+          opponent.ResetMove(m)
+        else:
+          filteredPossibleMoves.append(move)
+
+      self.ResetMove(move)
+
+    if len(filteredPossibleMoves) == 0:
+      return [possibleMoves[0]]
+
+    return filteredPossibleMoves
+
+
+  def RandomMove(self, game, opponent):
+    possibleMoves = self.PossibleMoves(game, opponent.positions)
+    move = random.choice(possibleMoves)
+    self.SetMove(move)
+    return move
+
+
+  def BestMoves(self, game, opponent):
+    possibleMoves = self.__twoMovesFilter(game, opponent)
+    if len(possibleMoves) == 1:
+      bestMoves = [possibleMoves[0]]
+      return bestMoves
+
+    maxEval = float('-inf')
+    minEval = float('inf')
+    bestMoves = list()
+
+    for move in possibleMoves:
+      print(move)
+      self.SetMove(move)
+
+      eval = minimax(game, 14, float('-inf'), float('inf'), not self.isP1)
+      print(eval)
+      if self.isP1:
+        if eval == maxEval:
+          bestMoves.append(move)
+        elif eval > maxEval:
+          maxEval = eval
+          bestMoves = [move]
+      else:
+        if eval == minEval:
+          bestMoves.append(move)
+        elif eval < minEval:
+          minEval = eval
+          bestMoves = [move]
+
+      self.ResetMove(move)
+
+    return bestMoves
+
+
+  def FastBestMoves(self, opponent):
+    p1Positions = self.positions
+    p2Positions = opponent.positions
+    if not self.isP1:
+      p1Positions = opponent.positions
+      p2Positions = self.positions
+
+    posString = GetPositionString(p1Positions, p2Positions)
+    if self.isP1:
+      return self.bestMoves[posString]['P1']
+    else:
+      return self.bestMoves[posString]['P2']
+
+
+  def Move(self, game, opponent):
+    if self.level == 'Random':
+      self.RandomMove(game, opponent)
+
+    if self.level == 'Advanced':
+      bestMoves = self.BestMoves(game, opponent)
+      move = random.choice(bestMoves)
+      self.SetMove(move)
+      return move
+
+    if self.level == 'FastAdvanced':
+      bestMoves = self.FastBestMoves(opponent)
+      move = tuple(random.choice(bestMoves))
+      self.SetMove(move)
+      return move
+
+
+class Game:
+  def __init__(self, player1: Player, player2: Player):
+    self.player1 = player1
+    self.player2 = player2
+
+    self.positions = (
+      'a',
+      'b',
+      'c',
+      'd',
+      'e',
+      'f',
+      'g',
+      'h',
+      'i',
+    )
+
+    self.connections = {
+      'a': ('d', 'f', 'i'),
+      'b': ('d', 'e', 'g'),
+      'c': ('e', 'f', 'h'),
+      'd': ('a', 'b', 'g', 'h'),
+      'e': ('b', 'c', 'h', 'i'),
+      'f': ('a', 'c', 'g', 'i'),
+      'g': ('b', 'd', 'f', 'h', 'i'),
+      'h': ('c', 'd', 'e', 'g', 'i'),
+      'i': ('a', 'e', 'f', 'g', 'h'),
+    }
+
+    self.winnerPositions = [
+      ('a', 'b', 'd'),
+      ('b', 'c', 'e'),
+      ('a', 'c', 'f'),
+      ('a', 'e', 'i'),
+      ('b', 'f', 'g'),
+      ('c', 'd', 'h'),
+      ('d', 'g', 'i'),
+      ('e', 'g', 'h'),
+      ('f', 'h', 'i'),
+    ]
+
+
+  def IsWinner(self, positions) -> bool:
+    if None in positions:
+      return False
+
+    positions = tuple(sorted(positions))
+    if positions in self.winnerPositions:
+      return True
+    return False
+
+
+  def IsP1Winner(self) -> bool:
+    return self.IsWinner(self.player1.positions)
+
+
+  def IsP2Winner(self) -> bool:
+    return self.IsWinner(self.player2.positions)
+
+
+  def Evaluation(self):
+    if self.IsP1Winner():
+      return 1
+    elif self.IsP2Winner():
+      return -1
+    else:
+      return 0
+
+
+def minimax(game: Game, depth, alpha, beta, isMaxPlayer):
+  if depth == 0:
+    return game.Evaluation()
+  if game.IsP1Winner():
+    return 1 + depth
+  if game.IsP2Winner():
+    return -(1 + depth)
+
+  if isMaxPlayer:
+    maxEval = float('-inf')
+    possibleMoves = game.player1.PossibleMoves(game, game.player2.positions)
+
+    for move in possibleMoves:
+      game.player1.SetMove(move)
+      eval = minimax(game, depth-1, alpha, beta, False)
+      game.player1.ResetMove(move)
+      maxEval = max(maxEval, eval)
+      alpha = max(alpha, eval)
+      if beta <= alpha:
+        break
+    return maxEval
+
+  else:
+    minEval = float('inf')
+    possibleMoves = game.player2.PossibleMoves(game, game.player1.positions)
+
+    for move in possibleMoves:
+      game.player2.SetMove(move)
+      eval = minimax(game, depth-1, alpha, beta, True)
+      game.player2.ResetMove(move)
+      minEval = min(minEval, eval)
+      beta = min(beta, eval)
+      if beta <= alpha:
+        break
+    return minEval
+
+##########################################################################################
+
+def AIvsAI(p1Level, p2Level, maxMoves=10):
+  p1 = Player(p1Level, True)
+  p2 = Player(p2Level, False)
+  game = Game(p1,p2)
+
+  # p1.SetMove(('x', 'a'))
+  # p2.SetMove(('x', 'i'))
+
+  for i in range(maxMoves):
+    ini = time.time()
+    print('\nPlayer 1:')
+    move = p1.Move(game, p2)
+    print('Best Move:', move)
+    print(time.time() - ini)
+    if game.IsP1Winner():
+      print(p1.positions)
+      print('Player 1 Win!')
+      break
+
+    ini = time.time()
+    print('\nPlayer 2:')
+    move = p2.Move(game, p1)
+    print('Best Move:', move)
+    print(time.time() - ini)
+    if game.IsP2Winner():
+      print(p2.positions)
+      print('Player 2 Win!')
+      break
+
+
+def HumanVsAi(level):
+  p1 = Player('Random', True)
+  p2 = Player(level, False)
+  game = Game(p1,p2)
+
+  while True:
+    oldPosition = input('Enter the old position: ')
+    newPosition = input('Enter the new position: ')
+    move = (oldPosition, newPosition)
+    p1.SetMove(move)
+    if game.IsP1Winner():
+      print(p1.positions)
+      print('Player 1 Win!')
+      break
+
+    move = p2.Move(game, p1)
+    print('Best Move:', move)
+    if game.IsP2Winner():
+      print(p2.positions)
+      print('Player 2 Win!')
+      break
+
+
+if __name__ == '__main__':
+  #AIvsAI('FastAdvanced', 'FastAdvanced')
+  HumanVsAi('FastAdvanced')
